51407cf4e5bf317cb43c298476a57c7dc177a29c
lightningflashh <thanhchi0429@gmail.com>
2024-12-20 22:21:46 +0700

    adding version for api

--- src/main/java/vn/hoidanit/jobhunter/controller/CompanyController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/CompanyController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.turkraft.springfilter.boot.Filter;
@@ -21,6 +22,7 @@ import vn.hoidanit.jobhunter.service.CompanyService;
 import vn.hoidanit.jobhunter.util.error.IdInvalidException;
 
 @RestController
+@RequestMapping("/api/v1")
 public class CompanyController {
 
     private final CompanyService companyService;
