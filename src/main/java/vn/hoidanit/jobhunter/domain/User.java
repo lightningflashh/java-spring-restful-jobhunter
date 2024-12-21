0565b98b9e37c02d4c59071f8d64ac23c9097902
lightningflashh <thanhchi0429@gmail.com>
2024-12-21 19:09:12 +0700

    update users domain

--- src/main/java/vn/hoidanit/jobhunter/domain/User.java
+++ src/main/java/vn/hoidanit/jobhunter/domain/User.java
@@ -1,12 +1,17 @@
 package vn.hoidanit.jobhunter.domain;
 
+import java.time.Instant;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
+import vn.hoidanit.jobhunter.util.constant.GenderEnum;
 
 @Entity
 @Table(name = "users")
@@ -22,4 +27,19 @@ public class User {
     private String email;
 
     private String password;
+
+    private String address;
+
+    @Enumerated(EnumType.STRING)
+    private GenderEnum gender;
+
+    private String refreshToken;
+
+    private Instant createdAt;
+
+    private Instant updatedAt;
+
+    private String createdBy;
+
+    private String updatedBy;
 }
