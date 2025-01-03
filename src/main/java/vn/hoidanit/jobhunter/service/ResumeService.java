75a0debfdf70a75659824af5428443d75fc01719
lightningflashh <thanhchi0429@gmail.com>
2025-01-03 17:10:30 +0700

    feat: implement Resume management with DTOs and repository for CRUD operations

---
+++ src/main/java/vn/hoidanit/jobhunter/service/ResumeService.java
@@ -0,0 +1,110 @@
+package vn.hoidanit.jobhunter.service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+
+import vn.hoidanit.jobhunter.domain.Resume;
+import vn.hoidanit.jobhunter.domain.response.ResCreateResumeDTO;
+import vn.hoidanit.jobhunter.domain.response.ResUpdateResumeDTO;
+import vn.hoidanit.jobhunter.domain.response.ResFetchResumeDTO;
+import vn.hoidanit.jobhunter.domain.response.ResultPaginationDTO;
+import vn.hoidanit.jobhunter.repository.JobRepository;
+import vn.hoidanit.jobhunter.repository.ResumeRepository;
+import vn.hoidanit.jobhunter.repository.UserRepository;
+
+@Service
+public class ResumeService {
+
+    private final ResumeRepository resumeRepository;
+    private final UserRepository userRepository;
+    private final JobRepository jobRepository;
+
+    public ResumeService(
+            ResumeRepository resumeRepository,
+            UserRepository userRepository,
+            JobRepository jobRepository) {
+        this.resumeRepository = resumeRepository;
+        this.userRepository = userRepository;
+        this.jobRepository = jobRepository;
+    }
+
+    public boolean checkResumeExistByUserAndJob(Resume resume) {
+        // check user and job by id
+        if (resume.getUser() == null || resume.getJob() == null) {
+            return false;
+        }
+
+        boolean userExists = this.userRepository.existsById(resume.getUser().getId());
+        boolean jobExists = this.jobRepository.existsById(resume.getJob().getId());
+
+        return userExists && jobExists;
+    }
+
+    public ResCreateResumeDTO create(Resume resume) {
+        resume = this.resumeRepository.save(resume);
+
+        ResCreateResumeDTO res = new ResCreateResumeDTO();
+        res.setId(resume.getId());
+        res.setCreatedBy(resume.getCreatedBy());
+        res.setCreatedAt(resume.getCreatedAt());
+
+        return res;
+    }
+
+    public Optional<Resume> fetchById(Long id) {
+        return this.resumeRepository.findById(id);
+    }
+
+    public ResFetchResumeDTO getResume(Resume resume) {
+        ResFetchResumeDTO res = new ResFetchResumeDTO();
+        res.setId(resume.getId());
+        res.setEmail(resume.getEmail());
+        res.setUrl(resume.getUrl());
+        res.setStatus(resume.getStatus());
+        res.setCreatedAt(resume.getCreatedAt());
+        res.setCreatedBy(resume.getCreatedBy());
+        res.setUpdatedAt(resume.getUpdatedAt());
+        res.setUpdatedBy(resume.getUpdatedBy());
+        res.setUser(new ResFetchResumeDTO.UserResume(resume.getUser().getId(), resume.getUser().getName()));
+        res.setJob(new ResFetchResumeDTO.JobResume(resume.getJob().getId(), resume.getJob().getName()));
+        return res;
+    }
+
+    public ResultPaginationDTO fetchAllResume(Specification<Resume> spec, Pageable pageable) {
+        Page<Resume> pageUser = this.resumeRepository.findAll(spec, pageable);
+        ResultPaginationDTO rs = new ResultPaginationDTO();
+        ResultPaginationDTO.Meta mt = new ResultPaginationDTO.Meta();
+        mt.setPage(pageable.getPageNumber() + 1);
+        mt.setPageSize(pageable.getPageSize());
+        mt.setPages(pageUser.getTotalPages());
+        mt.setTotal(pageUser.getTotalElements());
+        rs.setMeta(mt);
+        // remove sensitive data
+        List<ResFetchResumeDTO> listResume = pageUser.getContent()
+                .stream().map(item -> this.getResume(item))
+                .collect(Collectors.toList());
+        rs.setResult(listResume);
+        return rs;
+    }
+
+    public ResUpdateResumeDTO update(Resume resume) {
+        resume = this.resumeRepository.save(resume);
+
+        ResUpdateResumeDTO res = new ResUpdateResumeDTO();
+        res.setUpdatedBy(resume.getUpdatedBy());
+        res.setUpdatedAt(resume.getUpdatedAt());
+
+        return res;
+    }
+
+    public void delete(Long id) {
+        this.resumeRepository.deleteById(id);
+    }
+
+}
