2b0ac5e04c0d267a4e756f08d84ebadd73d9f84d
lightningflashh <thanhchi0429@gmail.com>
2025-01-06 22:30:21 +0700

    feat: add Subscriber entity and establish many-to-many relationship with Skill

--- src/main/java/vn/hoidanit/jobhunter/domain/Subscriber.java
+++ src/main/java/vn/hoidanit/jobhunter/domain/Subscriber.java
@@ -3,13 +3,15 @@ package vn.hoidanit.jobhunter.domain;
 import java.time.Instant;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
@@ -20,38 +22,36 @@ import lombok.Setter;
 import vn.hoidanit.jobhunter.util.SecurityUtils;
 
 @Entity
-@Table(name = "skills")
+@Table(name = "subscribers")
 @Getter
 @Setter
-public class Skill {
+public class Subscriber {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @NotBlank(message = "Tên kỹ năng không được để trống")
+    @NotBlank(message = "email không được để trống")
+    private String email;
+
+    @NotBlank(message = "name không được để trống")
     private String name;
 
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JsonIgnoreProperties(value = { "subscribers" })
+    @JoinTable(name = "subscriber_skill", joinColumns = @JoinColumn(name = "subscriber_id"), inverseJoinColumns = @JoinColumn(name = "skill_id"))
+    private List<Skill> skills;
+
     private Instant createdAt;
-
     private Instant updatedAt;
-
     private String createdBy;
-
     private String updatedBy;
-
-    @ManyToMany(fetch = FetchType.LAZY, mappedBy = "skills")
-    @JsonIgnore
-    private List<Job> jobs;
-
-    @ManyToMany(fetch = FetchType.LAZY, mappedBy = "skills")
-    @JsonIgnore
-    private List<Subscriber> subscribers;
 
     @PrePersist
     public void handleBeforeCreate() {
         this.createdBy = SecurityUtils.getCurrentUserLogin().isPresent() == true
                 ? SecurityUtils.getCurrentUserLogin().get()
                 : "";
+
         this.createdAt = Instant.now();
     }
 
@@ -60,6 +60,8 @@ public class Skill {
         this.updatedBy = SecurityUtils.getCurrentUserLogin().isPresent() == true
                 ? SecurityUtils.getCurrentUserLogin().get()
                 : "";
+
         this.updatedAt = Instant.now();
     }
+
 }
